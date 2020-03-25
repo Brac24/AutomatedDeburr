f85ae4e8c30caad83004b8101a78ddde8146cb9e
Christian Bracamontes <brac24@live.com>
2020-03-25 14:33:34 -0700

    changed ui

--- DeburrController.py
+++ DeburrController.py
@@ -7,7 +7,7 @@ class DeburrController:
     def __init__(self, name='deburr controller'):
         self.name = name
         self.motor_controller = None
-        self.led = LED(17)
+        self.led = LED(17) #This is gpio 17 on raspberry pi 3B+ this will control a relay that will provide power to AC motors
 
     def startup(self):
         """Initializes connection of main controller with the motor controller"""

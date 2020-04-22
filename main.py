fa7263e38f40ec8ed10309f6373e26e5278a24f3
Christian Bracamontes <brac24@live.com>
2020-04-22 08:33:22 -0700

    test change

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def start(self):
         t = threading.Thread(target=self.deburr_controller.start_deburr, args=(self.operation_time_entry.text(),))
+        t2 = threading.Thread(target=self.timer.start)
 
         if self.operation_time_entry.text() == "":
             error = "Please Enter an Operation Time"
@@ -52,10 +53,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
        # if error is None:
         self.max_deburr_time = int(self.operation_time_entry.text()) # Set total operation time
         self.reset_elapsed_time()                                    # Reset time at start of operation
-        self.timer.start()                                           # Start timer
+        t2.start()#self.timer.start()                                           # Start timer
         t.start()
+        t2.join()
         t.join()
-        if error != None:
+        if error is not None:
             self.display_error(error)
 
     def display_error(self, msg):
